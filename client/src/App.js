ae953504decb2094bf087f51be9cc1b68c0ee46d
Taylor Nyquist <tnyquist@gmail.com>
2020-11-02 20:43:03 -0600

    more setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
 function App() {
   return (
     <div className="App">
