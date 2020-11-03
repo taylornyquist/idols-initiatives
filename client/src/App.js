af0f6775ca9e1f6f127b102b6f7837ad8b669d75
Taylor Nyquist <tnyquist@gmail.com>
2020-11-03 09:50:17 -0600

    started navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar'
 
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { ApolloProvider } from '@apollo/react-hooks';
-import ApolloClient from 'apollo-boost';
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+// import { ApolloProvider } from '@apollo/react-hooks';
+// import ApolloClient from 'apollo-boost';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <header className="App-header">
         <h1>Idol Initiatives</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
---
+++ client/src/components/Navbar/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+
+const Navigation = () => {
+
+    return (
+        <>
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="#home">Idols Initiatives</Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#features">Features</Nav.Link>
+                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                </Nav>
+                <Form inline>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                    <Button variant="outline-info">Search</Button>
+                </Form>
+            </Navbar>
+        </>
+    );
+};
+
+export default Navigation
