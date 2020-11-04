c92b467c727f551b16c3a3b5ba991346ca3dff28
Jacob Daughtry <akutozo@gmail.com>
2020-11-03 21:45:02 -0600

    Adding resolvers.js mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,8 +4,48 @@ const { signToken } = require('../utils/auth');
 
 
 const resolvers = {
+    Query: {
 
-    
+
+
+    },
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+      
+            return { token, user };
+          },
+
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+      
+            if (!user) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+      
+            const correctPw = await user.isCorrectPassword(password);
+      
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+      
+            const token = signToken(user);
+      
+            return { token, user };
+          },
+
+        addIdol: async (parent, args) => {
+            const user = await Idol.create(args);
+      
+            return {  };
+          },
+        addCharity: async (parent, args) => {
+            const user = await Charity.create(args);
+      
+            return {  };
+          },
+    }
 };
 
 module.exports = resolvers;
