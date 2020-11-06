9386c89d285a9afeb8f6b8e7b0f92a59c44ca881
Taylor Nyquist <tnyquist@gmail.com>
2020-11-06 13:28:59 -0600

    working on redux categories

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,40 +1,43 @@
 import React, { useEffect } from 'react';
 import { Col, Row, Button, Container } from 'react-bootstrap';
-// import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
+import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
 import { useQuery } from '@apollo/react-hooks';
 // import { QUERY_CATEGORIES } from "../../utils/queries";
 import { useDispatch, useSelector } from 'react-redux';
-// import { idbPromise } from '../../utils/helpers';
+import { idbPromise } from '../../utils/helpers';
 
 function CategoryMenu() {
 
     const dispatch = useDispatch();
     const state = useSelector(state => state);
 
-    // this will eventually need to be moved to the server side seed
-    const categories = [
-        {
-            name: 'All Idols',
-            id: 0
-        },
-        {
-            name: 'Athletes',
-            id: 1
-        },
-        {
-            name: 'Musicians',
-            id: 2
-        },
-        {
-            name: 'Entrepreneurs',
-            id: 3
-        }
-    ];
-
-    const categoryData = ["test"];
-
-    // const { categories } = state;
+    const { categories } = state;
     // const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
+
+    // this will eventually need to be moved to the server side seed
+    // const categories = [
+    //     {
+    //         name: 'All Idols',
+    //         id: 0
+    //     },
+    //     {
+    //         name: 'Athletes',
+    //         id: 1
+    //     },
+    //     {
+    //         name: 'Musicians',
+    //         id: 2
+    //     },
+    //     {
+    //         name: 'Entrepreneurs',
+    //         id: 3
+    //     }
+    // ];
+
+    // faking the data: categoryData query from the server
+    // const categoryData = {
+    //     categories: ['All Idols', 'Athletes', 'Musicians', 'Entrepreneurs']
+    // };
 
     // useEffect(() => {
     //     if (categoryData) {
@@ -42,25 +45,30 @@ function CategoryMenu() {
     //             type: UPDATE_CATEGORIES,
     //             categories: categoryData.categories
     //         });
-    //         categoryData.categories.forEach(category => {
-    //             idbPromise('categories', 'put', category);
-    //         });
-    //     } else if (!loading) {
-    //         idbPromise('categories', 'get').then(categories => {
-    //             dispatch({
-    //                 type: UPDATE_CATEGORIES,
-    //                 categories: categories
-    //             });
-    //         });
-    //     }
-    // }, [categoryData, loading, dispatch]);
+    //         // categoryData.categories.forEach(category => {
+    //         //     idbPromise('categories', 'put', category);
+    //         // });
+    //     } 
+    //     // else if (!loading) {
+    //     //     idbPromise('categories', 'get').then(categories => {
+    //     //         dispatch({
+    //     //             type: UPDATE_CATEGORIES,
+    //     //             categories: categories
+    //     //         });
+    //     //     });
+    //     // }
 
-    // const handleClick = id => {
-    //     dispatch({
-    //         type: UPDATE_CURRENT_CATEGORY,
-    //         currentCategory: id
-    //     });
-    // };
+    //     // removed "loading" from the following array
+    // }, [categoryData, dispatch]);
+
+    const handleClick = id => {
+        dispatch({
+            type: UPDATE_CURRENT_CATEGORY,
+            currentCategory: id
+        });
+        console.log("clicked " + id);
+        console.log(state);
+    };
 
     return (
         // <div>
@@ -77,24 +85,20 @@ function CategoryMenu() {
         //     ))}
         // </div>
         <>
-        <h4 className="mb-3 ml-4">Choose a Category:</h4>
+            <h4 className="mb-3 ml-4">Choose a Category:</h4>
             <Container className="mb-3">
                 <Row>
-                    
-                </Row>
-
-                <Row>
-                    
-                        {categories.map(category => (
-                            <Col md={3} key={category.id}>
+                    {categories.map(category => (
+                        <Col md={3} key={category.id}>
                             <Button className="mr-3 mb-3 px-3" variant="secondary" block onClick={() => {
-                                console.log("clicked" + category.name);
+                                // console.log("clicked" + category.name);
+                                handleClick(category.id);
                             }}>
                                 {category.name}
                             </Button>
-                            </Col>
-                        ))}
-                    
+                        </Col>
+                    ))}
+
                 </Row>
             </Container>
         </>
--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -10,15 +10,32 @@ import {
     // TOGGLE_CART
 } from "./actions";
 
-const initialState = {
-
-    categories: [],
-    currentCategory: ''
+const defaultState = {
+    categories: [
+        {
+            name: 'All Idols',
+            id: 0
+        },
+        {
+            name: 'Athletes',
+            id: 1
+        },
+        {
+            name: 'Musicians',
+            id: 2
+        },
+        {
+            name: 'Entrepreneurs',
+            id: 3
+        }
+    ],
+    currentCategory: '0'
 };
 
-export const reducers = (state = initialState, action) => {
+export const reducers = (state = defaultState, action) => {
     switch (action.type) {
 
+        // if action type value is the value of `UPDATE_CATEGORIES`, return a new state object with an updated categories array
         case UPDATE_CATEGORIES:
             return {
                 ...state,
