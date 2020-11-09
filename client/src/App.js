847b4d6cb00ff561e93825981144b9b0285488ad
Jacob Daughtry <akutozo@gmail.com>
2020-11-08 18:33:22 -0600

    Updated App.js, confirmed token hitting server console

--- client/src/App.js
+++ client/src/App.js
@@ -16,8 +16,16 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:3001/graphql'
-});
+  request: (operation) => {
+    const token = localStorage.getItem('id_token')
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    })
+  },
+  uri: 'http://localhost:3001/graphql',
+})
 
 
 function App() {
