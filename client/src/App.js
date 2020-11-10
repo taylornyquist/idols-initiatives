76651323dff3029ac53f9a59bbf4e16bc3d425b2
Taylor Nyquist <tnyquist@gmail.com>
2020-11-10 09:45:30 -0600

    heroku 11

--- client/src/App.js
+++ client/src/App.js
@@ -31,7 +31,8 @@ const client = new ApolloClient({
       }
     })
   },
-  uri: 'http://localhost:3001/graphql',
+  // uri: 'http://localhost:3001/graphql',
+  uri: '/graphql',
 })
 
 function App() {
