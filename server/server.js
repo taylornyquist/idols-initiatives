cf55df5528ad2e6207b685ba9de272a9259c3b73
Taylor Nyquist <tnyquist@gmail.com>
2020-11-10 09:16:34 -0600

    heroku 9?

--- server/server.js
+++ server/server.js
@@ -23,10 +23,10 @@ app.use(express.json());
 // Serve up static assets
 // app.use('/images', express.static(path.join(__dirname, '../client/images')));
 
-// // Serve up static assets
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static(path.join(__dirname, '../client/build')));
-// };
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+};
 
 // Not sure about this...
 // app.get("/service-worker.js", (req, res) => {
