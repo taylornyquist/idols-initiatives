4fb1f9f00c3d5895e3fe353d75c769a388fe1890
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-11-10 20:07:47 -0600

    image added to saveIdol mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -21,3 +21,17 @@ export const ADD_USER = gql`
     }
   }
 `;
+
+export const SAVE_IDOL= gql `
+  mutation saveIdol($name: String!, $charity: String!, $description: String!, $hometown: String!, $charity_url: String! $image: String  ){
+    saveIdol(name: $name, charity: $charity, description: $description, hometown: $hometown, charity_url: $charity_url, image: $image){
+      name
+      charity
+      description
+      hometown
+      charity_url
+      twitter_url
+      image
+  }
+}
+`
