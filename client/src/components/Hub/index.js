eafa5e0e3aa4c2eb30db3aee0e265690cfdc5e88
Taylor Nyquist <tnyquist@gmail.com>
2020-11-10 21:39:04 -0600

    passed the ball to mike, might not use this branch

--- client/src/components/Hub/index.js
+++ client/src/components/Hub/index.js
@@ -11,17 +11,14 @@ import { useDispatch, useSelector } from 'react-redux';
 import { QUERY_MY_IDOLS } from '../../utils/queries';
 
 const Hub = () => {
-
     // change this to QUERY_MY_IDOLS or QUERY_ME???
     const { loading, data } = useQuery(QUERY_MY_IDOLS);
     const myIdols = data?.me.idols;
     console.log(myIdols);
-
     const dispatch = useDispatch();
     const state = useSelector(state => state);
     const { currentCategory } = state;
     console.log(currentCategory);
-
     function filterIdols() {
         if (!currentCategory || currentCategory === "clear") {
             return myIdols;
@@ -29,28 +26,21 @@ const Hub = () => {
         const filteredIdols = myIdols.filter(idol => idol.idol_category._id === currentCategory);
         return filteredIdols;
     };
-
     const removeFromHub = () => {
         console.log("click");
         // insert dispatch to remove from user's hub here
     };
-
     return (
-
         <>
             <Jumbotron className="py-4">
                 <h1>My Hub</h1>
             </Jumbotron>
-
             <CategoryMenu />
-
             <Container fluid>
                 <Row className="ml-auto mr-auto">
-
                     {myIdols ? (
                         <>
                             {filterIdols().map((idol) => (
-
                                 <CardDeck sm={12} md={6} lg={4} xl={3} className=" ml-auto mr-auto" key={idol.name}>
                                     <Card className="mr-4 mt-2 mb-4" style={{ width: '20rem' }}>
                                         <Card.Img variant="top" src={`/images/${idol.image}`} />
@@ -66,18 +56,15 @@ const Hub = () => {
                                             <Card.Link href={idol.charity_url} target="_blank" rel="noopener noreferrer">
                                                 Learn More
                                                 </Card.Link>
-
                                             {idol.twitter_url && (
                                                 <Card.Link className="float-right" href={idol.twitter_url} target="_blank" rel="noopener noreferrer">
                                                     <TwitterIcon />
                                                 </Card.Link>
                                             )}
-
                                         </Card.Body>
                                         <Button onClick={removeFromHub} variant="info">Remove From Hub</Button>{' '}
                                     </Card>
                                 </CardDeck>
-
                             ))}
                         </>
                     ) : (
@@ -91,11 +78,9 @@ const Hub = () => {
                                 </Spinner>
                             </Container>
                         </> : null}
-
                 </Row>
             </Container>
         </>
     );
 };
-
 export default Hub;
